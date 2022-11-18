49f2258dc1fb3f9b3bed90c2ab362b0439f73bbb
William Berbet <wiiliamb@hotmail.com>
2022-11-17 21:43:37 -0300

    finalizado design e func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import './index.css'
+import Adicionar from './Assets/adicionar.svg'
+import Remover from './Assets/remover.png'
+import Logo from './Assets/logo.png'
 
 function App() {
   const [counter, setCounter] = useState(0)
@@ -17,32 +20,38 @@ function App() {
     <div className='container'>
 
       <div>
+        <img src={Logo} alt="" className='logo' />
+      </div>
+
+      <div className='pergunta'>
         <h1>quantos pedaços você já comeu?</h1>
       </div>
 
       <div>
-        <h4>
+        <h4 className='frase1'>
           não importa, o Lucas vai comer mais que você
         </h4>
 
-        <h5>
+        <h5 className='frase2'>
           o William também!
         </h5>
       </div>
 
-      <div>
+      <div className='contador'>
         {counter}
       </div>
 
-
-      <h6>
-        #praticx10anos
-      </h6>
-
-      <div>
-        <button className='adicionar' onClick={handleClick1}>+</button>
-        <button className='remover' onClick={handleClick2}>-</button>
+      <div className='buttons'>
+        <a href={handleClick2} onClick={handleClick1}><img src={Adicionar} alt="" className='adicionar' /></a>
+        <a href={handleClick2} onClick={handleClick2}><img src={Remover} alt="" className='remover' /></a>
       </div>
+
+      <div className='hashtag'>
+        <h6>
+          #praticx10anos
+        </h6>
+      </div>
+
 
 
     </div>
